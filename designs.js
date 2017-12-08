3d31d1ebaf1215b76c3f83235577321ce833691a
Marcin <andrzejewski.m4@gmail.com>
2017-12-08 13:37:01 +0100

    Cell coloring
    Code cleaning, added pointer and color to each cell.

--- designs.js
+++ designs.js
@@ -1,30 +1,24 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-
 var submit = $("#submitButton");
 var colorPicker = $("#colorPicker");
 var gridTable = $("#pixelCanvas");
 
 function makeGrid(event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevent of refreshing site
 
     var grid = $("table");
     grid.empty();
-    var rows = $("#inputHeight").val(); // gets value from height
-    var cols = $("#inputWidth").val(); // gets value from width
+    var gridRows = $("#inputHeight").val(); // gets value from height
+    var gridCols = $("#inputWidth").val(); // gets value from width
 
 
 
-    //create rows
-    for (var i = 0; i < rows; i++) {
+    // Create rows
+    for (var i = 0; i < gridRows; i++) {
         grid.append("<tr></tr>");
 
-        // create cols
+        // Create cols
         var td = $("tr:last-child");
-        for (var j = 0; j < cols; j++) {
+        for (var j = 0; j < gridCols; j++) {
             td.append("<td></td>")
         }
     }
@@ -33,15 +27,20 @@ function makeGrid(event) {
 // There will be color picker
 colorPicker.change(function () {
     var pickedColor = colorPicker.val(); // gets value of picked color
-    $("td").css("background-color", pickedColor);
-    console.log(">>>>>>>>>>>", pickedColor);
+        console.log(">>>>>>>>>>>", pickedColor);
 });
 
 // Color each cell
- gridTable.on("mousedown", function(){
-     var pickedColor = colorPicker.val();
-     $("td").css("background-color", pickedColor);
- });
+gridTable.on("mousedown mouseup", "td",  function(e){
+     if (e.type === "mousedown") {
+         var pickedColor = colorPicker.val();
+         $(this).css("background-color", pickedColor);
+     }
+});
 
+ // Changed coursor type to pointer on grid
+gridTable.on("mousemove", function(){
+        $(this).css("cursor", "pointer");
+});
 
 submit.on("click", makeGrid);
